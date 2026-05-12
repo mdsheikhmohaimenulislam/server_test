9731baeeccc1c254460be6fe4e5f6b77c9ccaf57
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-12 22:14:37 +0600

    Building GET Method Operations && single get product find successfully done

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,7 +8,13 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
+  //   product => product/1 => ['',"product",'1']
+  const urlParts = url?.split("/");
+  // console.log(urlParts);
+  const id = urlParts && urlParts[1] === "product" ? Number(urlParts[2]) : null;
+  //   console.log(id);
 
+  //*..................GET All products get method..................
   if (url === "/product" && method === "GET") {
     const products = readProduct();
     res.writeHead(200, {
@@ -15,8 +22,23 @@ export const productController = (
     });
     res.end(
       JSON.stringify({
-        message: "Product retRived Successfully",
+        message: "Products retRived Successfully",
         data: products,
+      }),
+    );
+  }
+  //*..................GET Single product get method..................
+  else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id === id);
+    // console.log(product);
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(
+      JSON.stringify({
+        message: "Product retRived Successfully",
+        data: product,
       }),
     );
   }
---
+++ src/types/product.type.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+  id: number;
+  username: string;
+  location: string;
+  city: string;
+  productName: string;
+  price: number;
+}
