291ea7548b2dd5d30127e8acb98c5101d5f781c1
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-13 15:16:32 +0600

    Perfecting PUT Method Implementation

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -66,5 +66,34 @@ export const productController = async (
         data: newProduct,
       }),
     );
+  } else if (method === "PUT" && id !== null) {
+    // Updated product by PUT method
+    const body = await parseBody(req);
+    const products = readProduct();
+
+    const index = products.findIndex((p: IProduct) => p.id === id);
+    // console.log(index);
+    if (index < 0) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Product not found!",
+          data: null,
+        }),
+      );
+    }
+
+    // console.log(products[index]);
+    products[index] = { id: products[index].id, ...body };
+
+    insertProduct(products);
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product updated successfully!",
+        data: products[index],
+      }),
+    );
   }
 };
--- src/utility/parseBody.ts
+++ src/utility/parseBody.ts
@@ -8,7 +8,7 @@ export const parseBody = (req: IncomingMessage): Promise<any> => {
     });
     req.on("end", () => {
       try {
-        resolve(JSON.parse(body));
+       resolve(body ? JSON.parse(body) : {});
       } catch (error) {
         reject(error);
       }
